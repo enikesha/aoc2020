62976e8e92e40f7a0d6cc38fd6b4eaedfa818620
Innocenty Enikeew <enikesha@gmail.com>
2020-12-17 18:25:52 +0300

    day17: Generic solution

--- day17.pas
+++ day17.pas
@@ -1,113 +1,179 @@
 { fpc day17.pas && ./day17 < day17_input.txt }
-program day17;
+Program day17;
+
+Uses math;
 
 const
-   Size  = 20;
-   Steps = 6;
+   Steps     = 6;
+   N         = 4;
+   BoardBits = 5;
+
+   AxisMax   = 1 shl BoardBits;
+   LastIndex = 1 shl (N*BoardBits) - 1;
+   Axes : array [1..6] of char = ('X', 'Y', 'Z', 'W', 'V', 'Y');
 
 type
    StatusType = (Inactive, Active);
-   BoardType  = array[-Size..Size,-Size..Size,-Size..Size] of StatusType;
+   DimType    = 1..N;
+   AxisType   = 0..AxisMax-1;
+   IndexType  = 0..LastIndex;
+   BoardType  = array [0..LastIndex] of StatusType;
+   PointType  = array [1..N] of AxisType;
 
 var
    Board : BoardType;
-   X,Y,Z : Integer;
+   I     : IndexType;
+   Dim   : DimType;
 
 procedure Clear(var NewBoard : BoardType);
 begin
-   for Z := -Size to Size do
-      for Y := -Size to Size do
-         for X := -Size to Size do
-            NewBoard[X,Y,Z] := Inactive;
+   for I in IndexType do NewBoard[I] := Inactive;
 end;
 
 procedure ReadBoard;
 var
-   Line  : String;
-   C     : Char;
+   Line : String;
+   C    : Char;
+   Off  : Integer;
 begin
    Clear(Board);
 
-   Y := 0;
-   Z := 0;
+   { Middle of the board }
+   I := AxisMax shr 1;
+   for Dim := N-1 downto 1 do
+      I := (I shl BoardBits) + (AxisMax shr 1);
    while not eof do
    begin
       readln(Line);
-      X := 0;
+      Off := 0;
       for C in Line do
       begin
-         if C = '#' then Board[X,Y,Z] := Active;
-         X := succ(X)
+         if C = '#' then Board[I+Off] := Active;
+         Inc(Off);
       end;
-      Y := succ(Y)
+      I := I + AxisMax
+   end
+end;
+
+function GetPoint(I : IndexType) : PointType;
+var
+   Point : PointType;
+begin
+   for Dim := 1 to N do
+   begin
+      Point[Dim] := I mod AxisMax;
+      I := I shr BoardBits
+   end;
+
+   GetPoint := Point
+end;
+
+function GetIndex(Point : PointType) : IndexType;
+var
+   I : IndexType;
+begin
+   I := Point[N];
+   for Dim := N-1 downto 1 do
+      I := (I shl BoardBits) + Point[Dim];
+
+   GetIndex := I
+end;
+
+procedure WritePoint(Prefix : String; Point : PointType);
+begin
+   write(Prefix);
+   for Dim in DimType do
+   begin
+      write(Axes[Dim], ' = ', Point[Dim] - (AxisMax shr 1));
+      if Dim = N then
+         writeln
+      else
+         write(', ');
    end
 end;
 
 procedure WriteBoard;
 var
-   MinX,MaxX : Integer;
-   MinY,MaxY : Integer;
-   MinZ,MaxZ : Integer;
+   MinPoint, MaxPoint, LastPoint, Point : PointType;
+   InBounds                             : Boolean;
 begin
-   MinY := Size;
-   MaxY := -Size;
-   MinX := Size;
-   MaxX := -Size;
-   MinZ := Size;
-   MaxZ := -Size;
-
-   for Z := -Size to Size do
-      for Y := -Size to Size do
-         for X := -Size to Size do
-            if Board[X,Y,Z] = Active then
-            begin
-               if X < MinX then MinX := X;
-               if X > MaxX then MaxX := X;
-               if Y < MinY then MinY := Y;
-               if Y > MaxY then MaxY := Y;
-               if Z < MinZ then MinZ := Z;
-               if Z > MaxZ then MaxZ := Z;
-            end;
-
-   for Z := MinZ to MaxZ do
+   for Dim in DimType do
    begin
-      writeln('z=', Z);
-      for Y := MinY to MaxY do
+      MinPoint[Dim] := AxisMax-1;
+      MaxPoint[Dim] := 0;
+      LastPoint[Dim] := 0
+   end;
+
+   for I in IndexType do
+      if Board[I] = Active then
       begin
-         for X := MinX to MaxX do
-            if Board[X,Y,Z] = Active then
-               write('#')
-            else
-               write('.');
-         writeln;
+         Point := GetPoint(I);
+         for Dim in DimType do
+         begin
+            if Point[Dim] < MinPoint[Dim] then MinPoint[Dim] := Point[Dim];
+            if Point[Dim] > MaxPoint[Dim] then MaxPoint[Dim] := Point[Dim]
+         end
       end;
-      writeln
+
+   for I := GetIndex(MinPoint) to GetIndex(MaxPoint) do
+   begin
+      Point := GetPoint(I);
+      InBounds := true;
+      for Dim in DimType do
+         if (Point[Dim] < MinPoint[Dim]) or (Point[Dim] > MaxPoint[Dim]) then InBounds := false;
+      if InBounds then
+      begin
+         for Dim := N downto 3 do
+            if Point[Dim] <> LastPoint[Dim] then
+            begin
+               writeln;
+               write(Axes[Dim], '=', Point[Dim] - (AxisMax shr 1))
+            end;
+         if Point[2] <> LastPoint[2] then Writeln;
+         if Board[I] = Active then
+            write('#')
+         else
+            write('.');
+         LastPoint := Point;
+      end;
    end;
    writeln
 end;
 
 procedure Simulate;
 var
-   NextBoard : BoardType;
-   Neighbors : Integer;
-   DX,DY,DZ  : Integer;
+   NewBoard   : BoardType;
+   NeighPoint : PointType;
+   Point      : PointType;
+   J, K       : IndexType;
+   Neighbors  : Integer;
 begin
-   Clear(NextBoard);
+   Clear(NewBoard);
 
-   for Z := -Size+1 to Size-1 do
-      for Y := -Size+1 to Size-1 do
-         for X := -Size+1 to Size-1 do
-            begin
-               Neighbors := 0;
-               for DX := -1 to 1 do
-                  for DY := -1 to 1 do
-                     for DZ := -1 to 1 do
-                        if (not ((DX = 0) and (DY = 0) and (DZ = 0))) and (Board[X+DX,Y+DY,Z+DZ] = Active) then
-                           Neighbors := succ(Neighbors);
+   for I in IndexType do
+   begin
+      Point := GetPoint(I);
+      Neighbors := 0;
 
-               if (Neighbors = 3) or ((Neighbors = 2) and (Board[X,Y,Z] = Active)) then NextBoard[X,Y,Z] := Active;
-            end;
-   Board := NextBoard;
+      for J := 0 to (3 ** N)-1 do
+      begin
+         K := J;
+         for Dim in DimType do
+         begin
+            NeighPoint[Dim] := Max(Min(Point[Dim] + (K mod 3) - 1, AxisMax-1), 0);
+            K := K div 3
+         end;
+         if Board[GetIndex(NeighPoint)] = Active then Inc(Neighbors);
+      end;
+
+      if Board[I] = Active then
+      begin
+         if (Neighbors = 3) or (Neighbors = 4) then NewBoard[I] := Active;
+      end
+      else if Neighbors = 3 then NewBoard[I] := Active;
+   end;
+
+   Board := NewBoard;
 end;
 
 function CountBoard : Integer;
@@ -115,10 +181,8 @@ var
    Counter : Integer;
 begin
    Counter := 0;
-   for Z := -Size to Size do
-      for Y := -Size to Size do
-         for X := -Size to Size do
-            if Board[X,Y,Z] = Active then Counter := succ(Counter);
+   for I in IndexType do
+      if Board[I] = Active then Inc(Counter);
 
    CountBoard := Counter
 end;
@@ -134,7 +198,7 @@ begin
    for Step := 1 to Steps do
    begin
       Simulate;
+      WriteBoard;
       writeln('Step = ', Step, ', Count = ', CountBoard);
-      {WriteBoard;}
    end;
 end.
